799a372363cc6132e6e02208dc5d782538f5be67
JosephineEck <40391330+JosephineEck@users.noreply.github.com>
2018-08-28 15:11:14 +0200

    Platform
    Erstellung und Zerstörung einer Platform.

---
+++ DestroyPlatform.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Wenn der Spieler die Platform berührt, verschwindet die Plattform durch Änderung der Transparenz nach und nach.
+ * Josephine Eckhoff
+**/
+
+public class DestroyPlatform : MonoBehaviour {
+
+    public GameObject Player;
+    public float duration = 2.0f;
+    private bool fade = false;
+    private float startTime = 0.0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player) {
+            startTime = Time.time;
+            Destroy(gameObject, duration);
+            fade = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (fade)
+        {
+            Color col = gameObject.GetComponent<Renderer>().material.color;
+            col.a = 1 - ((Time.time-startTime)/duration);
+            gameObject.GetComponent<Renderer>().material.color = col;
+
+        }
+    }
+
+
+
+
+}
---
+++ Platform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Plattform wird erzeugt
+ * Josephine Eckhoff
+ **/
+
+public class Platform : MonoBehaviour {
+
+    public GameObject platform;
+    int x = -211;
+    int y = 10;
+    int z = 73;
+    // Use this for initialization
+    void Start()
+    {
+        platform = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        platform.transform.localScale = new Vector3(7.0f, 0.5f, 7.0f);
+        platform.transform.Translate(x, y, z);
+        platform.GetComponent<Renderer>().material.color = new Color(0.0f, 1.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
