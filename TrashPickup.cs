26e71e7bb8979465d6fd3e5bcc9400c4f60aa311
ard14n <ardianj4@gmail.com>
2018-08-28 20:49:38 +0200

    Revert "Alle Skripte in Ordner Scripts verschoben"
    This reverts commit de5abfb2732940ff68fa103dccd1b2081596a117.

---
+++ TrashPickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** Skript für Verhalten beim "Aufheben" der Pickups und Aufzählen des Scores
+ * 
+ * Marc Rosenkranz
+ * */
+
+public class TrashPickup : MonoBehaviour {
+
+    public int value;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().AddScore(value);    // Übergabe der Punktzahl an Game Manager für UI
+            Destroy(gameObject);
+        }
+    }
+}
