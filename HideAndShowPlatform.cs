998a91db4828c8e0f6f2ee2172cde25cdd8d1297
JosephineEck <40391330+JosephineEck@users.noreply.github.com>
2018-08-28 15:26:09 +0200

    Platform verschwindet immer wieder
    Die Platform wird nach einigen Sekunden immer wieder ein- und ausgeblendet.

---
+++ HideAndShowPlatform.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Die Platform wird immer wieder ein- und ausgeblendet.
+ * Josephine Eckhoff 
+ **/
+
+public class HideAndShowPlatform : MonoBehaviour {
+    // Use this for initialization
+    IEnumerator PlatformHide() {
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        Collider collider = gameObject.GetComponent<Collider>();
+
+        if(renderer.enabled == true)
+        {
+            yield return new WaitForSeconds(2.0f);
+         
+            renderer.enabled = false;
+            collider.enabled = false;
+        }
+
+        else
+        {
+            yield return new WaitForSeconds(2.0f);
+         
+            renderer.enabled = true;
+            collider.enabled = true;
+        }
+        }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(PlatformHide());
+    }
+   
+
+}
