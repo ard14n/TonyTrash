93714d3a5f034491e9b118b7c4f2b4a50de296a5
JosephineEck <40391330+JosephineEck@users.noreply.github.com>
2018-08-28 21:26:58 +0200

    Platform
    Die Platform bewegt sich von einem Punkt zum nächsten. Der Player bewegt sich mit der Platform mit, während er auf ihr steht.

---
+++ scripts/MovingPlatform.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Die Platform bewegt sich von einem Startpunkt zu einem Endpunkt und wieder zurück.
+ * Josephine Eckhoff
+ **/
+
+public class MovingPlatform : MonoBehaviour {
+
+    public Transform Platform1;
+    public Transform PosA;
+    public Transform PosB;
+    public Vector3 newPos;
+    public int current;
+    public float reset = 3;
+
+	// Use this for initialization
+	void Start () {
+        MovePlatform();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Platform1.position = Vector3.Lerp (Platform1.position, newPos, Time.deltaTime);
+    }
+
+    void MovePlatform()
+    {
+        if (current == 1)
+        {
+            current = 2;
+            newPos = PosB.position;
+        }
+        else if (current == 2)
+        {
+            current = 1;
+            newPos = PosA.position;
+        }
+        else if (current == 0)
+        {
+            current = 2;
+            newPos = PosB.position;
+        }
+        Invoke ("MovePlatform", reset);
+    }
+}
---
+++ scripts/Stay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Die Platform wird zum "Parent" des Players, sobald dieser auf der Platform steht. 
+ * Somit bewegt sich der Player mit der Platform mit. 
+ * Josephine Eckhoff
+ **/
+
+public class Stay : MonoBehaviour {
+
+    public GameObject Player;
+
+   private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+    }
+
+   private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+}
