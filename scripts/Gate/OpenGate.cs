d20496aaf4efea8216a4ff75846e07fa3242c5b8
ard14n <ardianj4@gmail.com>
2018-08-29 23:18:15 +0200

    Öffnet Türen des Zaunes
    Öffnet die Türen des Zaunes im ersten Areal nach betätigen des Triggers.
    Angepasst auf den Zaun, da davor nur mit Cubes getestet wurde.

---
+++ scripts/Gate/OpenGate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGate : MonoBehaviour {
+
+    private LeftDoor LeftDoor;
+    private RightDoor RightDoor;
+    
+    
+    // Use this for initialization
+    void Start () {
+
+        LeftDoor = GameObject.Find("LeftPivot").GetComponent<LeftDoor>();
+        RightDoor = GameObject.Find("RightPivot").GetComponent<RightDoor>();
+		
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {   
+
+        if(other.gameObject.tag == "DoorOpenerSphere")
+        {
+            LeftDoor.setOpened();
+            RightDoor.setOpened();
+        }
+        
+
+    }
+}
