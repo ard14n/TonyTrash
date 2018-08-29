eb3187ee5cc46813fa8c935ad4cd0b69736a9c1e
ard14n <ardianj4@gmail.com>
2018-08-29 23:19:26 +0200

    Inventarsystem ScriptableObjects
    Angefangen mit dem Inventarsystem mit ScriptableObjects

---
+++ scripts/Item/Consumable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item/Consumable", fileName = "Generic Consumable")]
+public class Consumable : Item {
+
+    [Header("Consumable Properties")]
+    public Types ItemType;
+    public int amount;
+
+	public enum Types
+    {
+        HealthPU, 
+        MagnetPU, 
+        JumpPU,
+        BombPU
+    }
+}
---
+++ scripts/Item/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName="Item/Generic", fileName ="Generic File Name")]
+public class Item : ScriptableObject {
+
+    public string name = "New Item";
+    public string description = "Item Description";
+
+
+	
+}
