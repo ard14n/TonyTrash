2d2cf474eb97873713c335301e8144e163381f84
JosephineEck <40391330+JosephineEck@users.noreply.github.com>
2018-08-31 00:04:09 +0200

    Revert "Merge branch 'master' of https://github.com/ard14n/TonyTrash"
    This reverts commit 74efabe7ab916ea26e7a27b6a46bc3820cfe191d, reversing
    changes made to 3459446aa413cc03ab36d1ec06955d57295d8755.

--- scripts/EnemyBehavior.cs
+++ scripts/EnemyBehavior.cs
@@ -6,9 +6,15 @@ public class EnemyBehavior : MonoBehaviour {
 
     private CharacterController controller;
     private Vector3 lookRotation;
+
+    private bool moving;
     private bool enemyInRange;
+
     public float movementSpeed;
     public float sightRange;
+    public float minDistance;
+    public float maxDistance;
+
     public GameObject target;
 
     void Start () {
@@ -18,28 +24,33 @@ public class EnemyBehavior : MonoBehaviour {
 
     void Update () {
 
-        RaycastHit downR;
+        RaycastHit hit;
         Ray downRay = new Ray(transform.position, -Vector3.up);
-        Physics.Raycast(downRay, out downR, Mathf.Infinity);
-        float down = downR.distance-0.00001f;
+        Physics.Raycast(downRay, out hit, Mathf.Infinity);
+        float down = hit.distance-0.00001f;
         transform.position = new Vector3(transform.position.x, transform.position.y - down, transform.position.z);
+
 
         enemyInRange = false;
 
         Collider[] enemys = Physics.OverlapSphere (transform.position, sightRange);
 
-        foreach(Collider element in enemys)
-        {
-            if (element.gameObject.tag == "Player")
+            foreach(Collider element in enemys)
             {
-                enemyInRange = true;
-                target = element.gameObject;
+                if (element.gameObject.tag == "Player") 
+                {
+                    enemyInRange = true;
+                    target = element.gameObject;
+                }
             }
-        }
-        if (enemyInRange)
+
+
+        if(enemyInRange)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 2f * Time.deltaTime);
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 2f*Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, transform.position.y , transform.position.z);
         }
+
     }
+
 }
