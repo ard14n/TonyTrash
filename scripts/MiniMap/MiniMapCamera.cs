8e647f248920e2fa6763b486a46cab4434f69793
ard14n <ardianj4@gmail.com>
2018-09-04 19:27:11 +0200

    MiniMap hinzugefügt und MiniMapKamera verbessert
    Kamera-Bug gefixt;
    Kamera folgt nun yPosition des Players

---
+++ scripts/MiniMap/MiniMapCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCamera : MonoBehaviour {
+
+    private GameObject player;
+    private Transform playerTransform;
+    public float cameraHeight = 200f;
+
+    void Start() {
+
+        player = GameObject.Find("Player");
+        playerTransform = player.transform;
+
+        this.gameObject.GetComponent<Camera>().fieldOfView = 90f;
+
+    }
+
+
+	void LateUpdate() {
+
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y+cameraHeight, playerTransform.position.z);
+        
+    }
+}
