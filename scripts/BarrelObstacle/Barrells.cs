538324e90a2d9a1e0fbc93abfadcf55c686634ac
ard14n <ardianj4@gmail.com>
2018-09-16 16:50:57 +0200

    Rollende Fässer hinzugefügt
    Vor dem Endgegnerabschnitt rollen nun Fässer den Weg hinunter, denen der Spieler entgegen läuft und ausweichen muss, damit er zum Endgegner kommt.
    Die Fässer werden oben zufällig auf der Z-Achse gespawnt. Die Fässer haben einen Rigidbody der auf der Z-Achse gesperrt ist, damit sie geradaus runterrollen. Bei Berührung mit der Straße am Fuße der Schräge werden sie zerstört.

---
+++ scripts/BarrelObstacle/Barrells.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrells : MonoBehaviour {
+
+	
+    void OnCollisionEnter(Collision other) {
+
+        if(other.gameObject.tag == "RoadBarrellStop"){
+
+            Destroy(gameObject);
+
+        }
+
+    }
+}
---
+++ scripts/BarrelObstacle/FassSpawner.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FassSpawner : MonoBehaviour {
+
+    public GameObject barrellPrefab;
+    private GameObject spawnedBarrell;
+    private float spawnDelay = 5f;
+
+    private float center = 385f;
+
+    public bool stop = false;
+
+    private Rigidbody barrellrigid;
+
+	// Use this for initialization
+	void Start () {
+
+
+        TransformBarrel();
+        ManipulateRigidbody();
+
+        InvokeRepeating("SpawnBarrell", 2f, spawnDelay);
+         
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void SpawnBarrell() {
+
+        Debug.Log("Ich spawne jetzt");
+        
+        Vector3 spawnPosition = new Vector3(-1170f, 430f, Random.Range(-9 + center, 9 + center));
+        spawnedBarrell = Instantiate(barrellPrefab, spawnPosition, transform.rotation);
+        
+
+        if (stop) {
+            CancelInvoke("SpawnBarrell");
+        }
+        
+    }
+
+    
+    
+    private void AddScriptToPrefab() {
+
+        if (barrellPrefab != null)
+            barrellPrefab.AddComponent<Barrells>();
+
+    }
+
+    private void ManipulateRigidbody() {
+
+        barrellrigid = barrellPrefab.GetComponent<Rigidbody>();
+        barrellrigid.constraints = RigidbodyConstraints.FreezePositionZ;
+
+    }
+
+    private void TransformBarrel() {
+
+        //barrellPrefab = CreateBarrell();
+        barrellPrefab.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+        barrellPrefab.transform.localScale = new Vector3(20f, 20f, 20f);
+
+    }
+
+
+
+
+}
